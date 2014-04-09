591e4443c21161fcdf91b9ccc5e033eb56404815
Gustavo Giráldez <ggiraldez@manas.com.ar>
2014-04-09 19:09:53 -0300

    Use Erlang's Poirot receiver.

--- recipes/receiver.rb
+++ recipes/receiver.rb
@@ -1,63 +1,91 @@
-include_recipe "golang"
+include_recipe "erlang::esl"
+include_recipe "build-essential"
 
-# Install deps
-case node['platform']
-when 'ubuntu'
-  package "uuid-dev"
-  include_recipe "zeromq"
-when 'redhat', 'centos'
-  package "zeromq-devel"
-else
-  raise "Unsupported platform: #{node['platform']}"
+package "git"
+package "zeromq-devel"
+
+remote_file "/usr/local/bin/rebar" do
+  source "https://github.com/rebar/rebar/wiki/rebar"
+  mode 0755
 end
 
-# Create user to run service
+app_dir = "/u/apps/poirot"
+
+# Create user to run the receiver service
 user node['poirot']['receiver']['user']
 
-# Register service in chef
-service "poirot_receiver" do
-  provider Chef::Provider::Service::Upstart
+# Create shared and deploy dirs writable by root only
+%w(/ /releases/ /shared/).each do |dir|
+  directory "#{app_dir}#{dir}" do
+    recursive true
+  end
 end
 
-# Set up upstart configuration
-template "poirot_receiver.conf" do
-  path "/etc/init/poirot_receiver.conf"
-  source "poirot_receiver.conf.erb"
+# Create shared and deploy dirs writable by cepheid receiver user
+%w(/shared/log/ /shared/pids/).each do |dir|
+  directory "#{app_dir}#{dir}" do
+    owner node['poirot']['receiver']['user']
+    recursive true
+  end
+end
+
+template "poirot.config" do
+  path "#{app_dir}/shared/poirot.config"
+  source "poirot.config.erb"
+  owner node['poirot']['receiver']['user']
+  mode 0600
+end
+
+# Register service in upstart
+template "poirot.conf" do
+  path "/etc/init/poirot.conf"
+  source "poirot.conf.erb"
   owner "root"
   group "root"
   mode 0644
-  notifies :stop, "service[poirot_receiver]"
-  notifies :start, "service[poirot_receiver]"
-  variables logdir: "/opt/poirot_receiver/shared/log", user: node['poirot']['receiver']['user']
+  variables(
+    user: node['poirot']['receiver']['user'],
+    app_dir: app_dir
+  )
 end
 
-# Configure receiver logs in upstart
-logrotate_app "poirot_receiver" do
-  path "/opt/poirot_receiver/shared/log/*.log"
+# Configure log rotate for application
+logrotate_app "poirot-receiver" do
+  path "#{app_dir}/shared/log/*.log"
   frequency :daily
   rotate 7
   options %w(missingok compress delaycompress notifempty copytruncate)
 end
 
-# Deploy poirot receiver application
-deploy_revision "/opt/poirot_receiver" do
-  repo "https://bitbucket.org/instedd/poirot_receiver.git"
-  create_dirs_before_symlink []
-  purge_before_symlink []
+# Allow ZMQ port through firewall
+simple_iptables_rule "poirot" do
+  rule "--proto tcp --dport #{node['poirot']['receiver']['port']}"
+  jump "ACCEPT"
+end
+
+# Trust bitbucket
+ssh_known_hosts_entry "bitbucket.org"
+
+# Deploy receiver application
+application "poirot" do
+  revision node['poirot']['revision'] if node['poirot']['revision']
+
+  path app_dir
+  repository "git@bitbucket.org:instedd/poirot_erlang.git"
+  deploy_key data_bag_item('deploy_keys', 'deploy_key')['private_key']
+  purge_before_symlink ["log", "tmp"]
+  symlinks "log" => "log", "tmp" => "tmp", "poirot.config" => "poirot.config"
+  restart_command "sudo stop poirot; sudo start poirot"
+
   symlink_before_migrate({})
-  symlinks({})
+  migrate false
+
   before_restart do
     bash "make" do
       cwd release_path
       flags "--login"
-      code "make"
+      code "ln -s . poirot; make"
     end
   end
-  notifies :restart, "service[poirot_receiver]"
 end
 
-# Allow conncetions to poirot receiver
-simple_iptables_rule "poirot_receiver" do
-  rule "--proto tcp --dport #{node['poirot']['receiver']['port']}"
-  jump "ACCEPT"
-end
