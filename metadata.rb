8d046aaee8a423877badda471cce7f103837acdf
Lucas Persson <lucas.persson@cepheid.se>
2017-03-20 10:48:27 +0100

    bump version since the tempalte file was updated

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'jwajnerman@manas.com.ar'
 license          'All rights reserved'
 description      'Installs/Configures poirot'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends "build-essential"
 depends "golang"
